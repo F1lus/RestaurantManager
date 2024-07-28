2d06b4e2938cc7d9ac15dfaa0aa45cf1d91894c8
F1lus <filimon.mark@gmail.com>
2024-07-28 15:24:21 +0200

    Add PreAuthorize

--- Backend/src/main/java/org/restaurantmanager/backend/util/seat/ISeatingService.java
+++ Backend/src/main/java/org/restaurantmanager/backend/util/seat/ISeatingService.java
@@ -3,6 +3,7 @@ package org.restaurantmanager.backend.util.seat;
 import org.restaurantmanager.backend.dto.seating.CreateSeatingRequest;
 import org.restaurantmanager.backend.dto.seating.ModifySeatingRequest;
 import org.restaurantmanager.backend.dto.seating.SeatingResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,9 +12,12 @@ public interface ISeatingService {
 
     List<SeatingResponse> getSeats();
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     void addSeat(final CreateSeatingRequest createSeatingRequest);
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     void modifySeat(final UUID id, final ModifySeatingRequest modifySeatingRequest);
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     void deleteSeat(final UUID id);
 }
